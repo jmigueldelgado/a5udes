6e8d66d5426f5b0756f43f8dd07bdae762480ad9
JM Delgado <jmigueldelgado@posteo.net>
2020-07-30 16:11:12 +0200

    tidy up import of hydrosheds

--- import_reservoirs.r
+++ import_reservoirs.r
@@ -8,30 +8,6 @@ library(furrr)
 
 CE=osmdata::getbb(place_name='Ceará', format_out = "sf_polygon") %>% slice(1)
 
-# catch=st_read('/home/delgado/Downloads/hydrosheds-317f5f6b1c07745a6086/hybas_sa_lev12_v1c')
-# catch_ce=catch %>% st_filter(CE)
-# rm(catch)
-#
-# catchment_geometry = catch_ce %>% dplyr::select(HYBAS_ID,NEXT_DOWN,SUB_AREA,UP_AREA) %>% st_transform(32724)
-#
-# save(catchment_geometry,file='data/catchment_geometry.RData')
-#
-# catchment_graph=catch2graph(catchment_geometry)
-#
-# save(catchment_graph,file='data/catchment_graph.RData')
-riv=st_read('/home/delgado/Downloads/HydroRIVERS_v10_sa_shp/HydroRIVERS_v10_sa_shp')
-riv_ce=riv %>% st_filter(CE)
-rm(riv)
-#
-# names(riv_ce)
-#
-river_geometry=riv_ce %>% dplyr::select(HYRIV_ID,NEXT_DOWN,CATCH_SKM,UPLAND_SKM,HYBAS_L12) %>% st_transform(32724)
-# save(river_geometry,file='data/river_geometry.RData')
-
-# nodes=riv2nodes(river_geometry)
-# river_graph=riv2graph(nodes,river_geometry)
-# save(river_graph,file='data/river_graph.RData')
-
 riv_i=river_geometry
 reservoir_geometry=allocate_reservoir_to_river(riv_i)
 save(reservoir_geometry,file='data/reservoir_geometry.RData')
